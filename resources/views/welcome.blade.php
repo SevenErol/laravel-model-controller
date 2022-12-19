bd037d2335c1af28013afa23451e429c6ef0f844
SevenErol <lorenzo.maggi1998@gmail.com>
2022-12-19 15:40:28 +0100

    card on page and exercise complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,17 +4,30 @@
 
 <h1 class="text-center">Hello world</h1>
 
-<ul>
 
-    @foreach($movies as $movie)
+<div class="container">
 
+    <div class="row row-cols-5">
 
-    <li>{{ $movie->title }}</li>
-
-    @endforeach
+        @foreach($movies as $movie)
 
 
+        <div class="col gy-3">
+            <div class="card text-center p-2">
+                <p>Titolo del film: {{ $movie->title }}</p>
+                <p>Titolo originale: {{ $movie->original_title }}</p>
+                <p>Nationality: {{ $movie->nationality }}</p>
+                <p>Data di uscita: {{ $movie->date}}</p>
+                <p>Voto: {{ $movie->vote }}</p>
+            </div>
+        </div>
 
-</ul>
+        @endforeach
+
+
+
+    </div>
+</div>
+
 
 @endsection
